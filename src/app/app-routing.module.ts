3218839564ca9c7ea50937f1a64edc3a5820665e
Dorian Barboza <dorianbarboza@gmail.com>
2019-09-17 17:40:49 -0500

    Avance pagina noticias

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
   { path: 'index', loadChildren: './paginas/index/index.module#IndexPageModule' },
+  { path: 'noticias', loadChildren: './paginas/noticias/noticias.module#NoticiasPageModule' },
 ];
 
 @NgModule({
---
+++ src/app/paginas/noticias/noticias.page.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-noticias',
+  templateUrl: './noticias.page.html',
+  styleUrls: ['./noticias.page.scss'],
+})
+export class NoticiasPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+  doRefresh(event) {
+    console.log('Begin async operation');
+
+    setTimeout(() => {
+      console.log('Async operation has ended');
+      event.target.complete();
+    }, 2000);
+  }
+
+}

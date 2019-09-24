745d968e17407c7738f1f6bfad486ed11cdd390d
Dorian Barboza <dorianbarboza@gmail.com>
2019-09-24 02:34:03 -0500

    elimiando page index

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,6 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
-  { path: 'index', loadChildren: './paginas/index/index.module#IndexPageModule' },
   { path: 'noticias', loadChildren: './paginas/noticias/noticias.module#NoticiasPageModule' },
 ];
 

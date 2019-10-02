542b2451281f8fbe46f77d36521c211273279a35
Dorian Barboza <dorianbarboza@gmail.com>
2019-10-02 08:48:52 -0500

    Modificando el splash de inicio de la app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,16 @@ import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
+import { Subscription, timer, from } from 'rxjs';
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss']
 })
 export class AppComponent {
+
+   showSplash = true; // <-- show animation
 
   public appPages = [
   {
@@ -41,6 +45,8 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+
+      timer(3000).subscribe(() => this.showSplash = false) // <-- hide animation after 3s
     });
   }
 }
